cdbe0402dd7a1518ada26972cca078b2115fa411
seohyunchoiii <fooodie@naver.com>
2024-06-11 21:50:57 +0900

    Make Buttons prop

---
+++ src/components/Buttons.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Container = styled.div``
+const Button = styled.button`
+  font-size: 14px;
+  font-weight: 500;
+  background-color: #f9f9f9;
+  border: 1px solid #dfe3e6;
+  border-radius: 5px;
+  padding: 2px 10px;
+  margin-right: 10px;
+  cursor: pointer;
+`
+
+const Buttons = (props) => {
+  return (
+    <Container>
+      <Button name>{props.name}</Button>
+    </Container>
+  )
+}
+
+export default Buttons
--- src/components/TaskForm.jsx
+++ src/components/TaskForm.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import styled from 'styled-components'
+import Buttons from './Buttons'
 
 const Container = styled.div`
   display: flex;
@@ -30,16 +31,6 @@ const Bottom = styled.div`
   display: flex;
   align-items: center;
   justify-content: space-between;
-`
-const Button = styled.button`
-  font-size: 14px;
-  font-weight: 500;
-  background-color: #f9f9f9;
-  border: 1px solid #dfe3e6;
-  border-radius: 5px;
-  padding: 2px 10px;
-  margin-right: 10px;
-  cursor: pointer;
 `
 
 const Options =[
@@ -100,10 +91,10 @@ const TaskForm = () => {
           />
           <Bottom>
             <div>
-            <Button>HTML</Button>
-            <Button>CSS</Button>
-            <Button>Javascript</Button>
-            <Button>React</Button>
+            <Buttons name="HTML"/>
+            <Buttons name="CSS"/>
+            <Buttons name="JavaScript"/>
+            <Buttons name="React"/>
             </div>
             
             <div>
